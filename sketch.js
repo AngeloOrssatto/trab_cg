f067396161a427e6d5e8dd4ea34905b3c209cdc0
Angelo Jose Orssatto <61063308+AngeloOrssatto@users.noreply.github.com>
2021-09-06 03:33:20 -0300

    add rotation

--- sketch.js
+++ sketch.js
@@ -1,4 +1,10 @@
-let angle = degrees_to_radians(30);
+let angleX = degrees_to_radians(0);
+let angleY = degrees_to_radians(0);
+let angleZ = degrees_to_radians(0);
+
+let dx = 0
+let dy = 0
+let dz = 0
 
 let index = 0
 let r = 0
@@ -9,7 +15,7 @@ let h = 0
 
 let vrp = []
 let p = []
-let vecU = [1,1,1]
+let vecU = []
 let vecV = []
 let vecN = []
 
@@ -107,6 +113,86 @@ function degrees_to_radians(degrees){
   return degrees * (pi/180);
 }
 
+function transformations(e) {
+  if(e.code === 'KeyA'){
+    // movimento em X - direita
+    dx+=1
+  }
+  if(e.code === 'KeyQ'){
+    // movimento em X - esquerda
+    dx-=1
+  }
+  if(e.code === 'KeyW'){
+    // movimento em Z - frente
+    dz+=1
+  }
+  if(e.code === 'KeyS'){
+    // movimento em Z - tras
+    dz-=1
+  }
+  if(e.code === 'KeyE'){
+    // movimento em Y - cima
+    dy+=1
+  }
+  if(e.code === 'KeyD'){
+    // movimento em Y - baixo
+    dy-=1
+  }
+
+  if(e.code === 'KeyR'){
+    // diminui em X
+  }
+  if(e.code === 'KeyF'){
+    // aumenta em X 
+  }
+  if(e.code === 'KeyT'){
+    // diminui em Z 
+  }
+  if(e.code === 'KeyG'){
+    // aumenta em Z 
+  }
+  if(e.code === 'KeyY'){
+    // diminui em Y 
+  }
+  if(e.code === 'KeyH'){
+    // aumenta em Y 
+  }
+
+  if(e.code === 'KeyU'){
+    // rotaciona em X - pos
+    angleX+=10
+    console.log(angleX)
+  }
+  if(e.code === 'KeyJ'){
+    // rotaciona em X - neg 
+    angleX-=10
+    console.log(angleX)
+  }
+  if(e.code === 'KeyI'){
+    // rotaciona em Z - pos
+    angleZ+=10
+    console.log(angleZ) 
+  }
+  if(e.code === 'KeyK'){
+    // rotaciona em Z - neg 
+    angleZ-=10
+    console.log(angleZ)
+  }
+  if(e.code === 'KeyO'){
+    // rotaciona em Y - pos
+    angleY+=10
+    console.log(angleY)
+  }
+  if(e.code === 'KeyL'){
+    // rotaciona em Y - neg
+    angleY-=10
+    console.log(angleY)
+  }
+
+}
+
+document.addEventListener('keypress', transformations);
+
 function setup() {
   createCanvas(1200, 900);
   
@@ -119,21 +205,39 @@ function draw() {
   translate(width / 2, height / 2);
 
   const rotationZ = [
-    [cos(angle), -sin(angle), 0],
-    [sin(angle), cos(angle), 0],
+    [Math.cos(angleZ), -Math.sin(angleZ), 0],
+    [Math.sin(angleZ), Math.cos(angleZ), 0],
     [0, 0, 1]
   ];
-
+  
   const rotationX = [
     [1, 0, 0],
-    [0, cos(angle), -sin(angle)],
-    [0, sin(angle), cos(angle)]
+    [0, Math.cos(angleX), -Math.sin(angleX)],
+    [0, Math.sin(angleX), Math.cos(angleX)]
+  ];
+  
+  const rotationY = [
+    [Math.cos(angleY), 0, Math.sin(angleY)],
+    [0, 1, 0],
+    [-Math.sin(angleY), 0, Math.cos(angleY)]
   ];
 
-  const rotationY = [
-    [cos(angle), 0, sin(angle)],
+  const translateZ = [
+    [1, 0, 0],
     [0, 1, 0],
-    [-sin(angle), 0, cos(angle)]
+    [0, 0, dz]
+  ];
+  
+  const translateX = [
+    [1, 0, dx],
+    [0, 1, 0],
+    [0, 0, 0]
+  ];
+  
+  const translateY = [
+    [1, 0, 0],
+    [0, 1, dy],
+    [0, 0, 0]
   ];
 
   let projected = [];
@@ -142,12 +246,15 @@ function draw() {
   for (let i = 0; i < points.length; i++) {
     
     let rotated = matmul(MatrixSRU2SRC, points[i])
-    //rotated = matmul(rotationY, rotated);
-    //rotated = matmul(rotationX, rotated);
-    //let rotated = matmul(rotationZ, points[i]);
+    rotated = matmul(rotationY, rotated);
+    rotated = matmul(rotationX, rotated);
+    rotated = matmul(rotationZ, rotated);
+    rotated = matmul(translateY, rotated);
+    rotated = matmul(translateX, rotated);
+    rotated = matmul(translateZ, rotated);
 
     const distance = 5
-    const z = 1/(distance-rotated.z)
+    //const z = 1/(distance-rotated.z)
 
     const projection = [
       [1, 0, 0],
@@ -158,7 +265,7 @@ function draw() {
     projected2d.mult(10);
     projected[i] = projected2d;
     //point(projected2d.x, projected2d.y);
-    angle+=0.0005
+    //angle+=0.0005
   }
 
   //console.log(projected)
